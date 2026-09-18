7499dc435302e08c61aefe6d4b1fe08314248648
Witterm0193 <witterm0193@student.faytechcc.edu>
2026-09-17 23:12:51 -0400

    Add files via upload

---
+++ P1LAB1_WitterMichael.py
@@ -0,0 +1,9 @@
+# Michael Witter
+# September 17, 2026
+# Assignment Name: P1LAB1 - Input/Output Basics
+# This program prompts the user for their first and last name
+# and prints a personalized welcome message.
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+print("Welcome, " + first_name + " " + last_name + "!")
